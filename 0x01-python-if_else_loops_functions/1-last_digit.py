1718424645695abf18e4063245b513fd2a11e377
Martin-Rota <martinrota5@gmail.com>
2023-12-11 16:12:47 -0800

    removes '\n'

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ else:
     last_digit = number % 10
 
 if number > 5:
-    print(f'Last digit of {number} is {last_digit} and is greater than 5\n')
+    print(f'Last digit of {number} is {last_digit} and is greater than 5')
 elif number == 0:
-    print(f'Last digit of {number} is {last_digit} and is 0\n')
+    print(f'Last digit of {number} is {last_digit} and is 0')
 elif number < 6 and number != 0:
-    print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0\n')
+    print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
