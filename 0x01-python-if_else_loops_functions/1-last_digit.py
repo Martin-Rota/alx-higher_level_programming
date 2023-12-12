b8d77e6b55828f8eac9c2561cd2a59ee7c1ad6e2
Martin-Rota <martinrota5@gmail.com>
2023-12-11 16:18:55 -0800

    fixes pycodestyle error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_digit = number % (-10)
+    x = number % (-10)
 else:
-    last_digit = number % 10
+    x = number % 10
 
 if number > 5:
-    print(f'Last digit of {number} is {last_digit} and is greater than 5')
+    print(f'Last digit of {number} is {x} and is greater than 5')
 elif number == 0:
-    print(f'Last digit of {number} is {last_digit} and is 0')
+    print(f'Last digit of {number} is {x} and is 0')
 elif number < 6 and number != 0:
-    print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
+    print(f'Last digit of {number} is {x} and is less than 6 and not 0')
