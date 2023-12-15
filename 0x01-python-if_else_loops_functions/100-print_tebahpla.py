e57c5d7e57793ab03b794ec24476dbfcd61959d8
Martin-Rota <martinrota5@gmail.com>
2023-12-14 20:00:28 -0800

    fixes .format requirement

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for letter in range(ord('z'), ord('a') - 1, -1):
-    print(f'{chr(letter)}' if letter % 2 == 0 else chr(letter - 32), end='')
+    print('{}'.format(chr(letter)) if letter % 2 == 0 else chr(letter - 32), end='')
