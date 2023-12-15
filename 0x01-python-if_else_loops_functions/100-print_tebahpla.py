a6b4ad1f7510b514d719bbb81c009f9057db5d7c
Martin-Rota <martinrota5@gmail.com>
2023-12-14 20:09:57 -0800

    fixes pycodestyle line length and indentation error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('z'), ord('a') - 1, -1):
-    print('{}'.format(chr(letter)) if letter % 2 == 0 else chr(letter - 32), end='')
+    print('{}'.format(chr(letter)) if letter % 2 == 0 else chr(letter - 32),
+          end='')
