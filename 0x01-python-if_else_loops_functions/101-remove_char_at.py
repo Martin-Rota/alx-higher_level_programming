5eed61f7aeb4185b2b3774581eab25323675dcbf
Martin-Rota <martinrota5@gmail.com>
2023-12-14 21:35:57 -0800

    first commit

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for letter in str:
-        if 0 <= n:
-            str_cpy = str[:n] + str[n + 1:]
-            return str_cpy
-        else:
-            return str
+    if 0 <= n < len(str):
+        str_cpy = str[:n] + str[n + 1:]
+        return str_cpy
+    else:
+        return str
