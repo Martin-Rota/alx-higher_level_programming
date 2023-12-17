38043b865a8c055ea7f79ebebea4abe583494037
Martin-Rota <martinrota5@gmail.com>
2023-12-16 18:29:55 -0800

    changes import syntax to from...

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
-    add_0.a = 1
-    add_0.b = 2
+    from add_0 import add
+    a = 1
+    b = 2
 
-print('{} + {}  =  {}'.format(add_0.a, add_0.b, add_0.add(1, 2)))
+print('{} + {}  =  {}'.format(a, b, add(1, 2)))
