0326a7e32204ee1b3bdf0fe9fc193e833f0869ca
Martin-Rota <martinrota5@gmail.com>
2023-12-17 20:34:19 -0800

    fix pycodestyle error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
         print('{} argument:'.format(len(argv) - 1))
         print('1: {}'.format(argv[1]))
     else:
-        print('{} arguments:'.format(len(argv) - 1) if len(argv) != 1 else '{} arguments.'.format(len(argv) - 1))
+        print('{} arguments:'.format(len(argv) - 1)
+              if len(argv) != 1 else '{} arguments.'.format(len(argv) - 1))
         for arg_value, argument in enumerate(argv[1:], start=1):
             print('{}: {}'.format(arg_value, argument))
