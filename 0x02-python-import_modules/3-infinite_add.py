b4492dfb626dc858050448498a733d8585a9c031
Martin-Rota <martinrota5@gmail.com>
2023-12-18 18:23:49 -0800

    make it print 0 when argument = 1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+    result = 0
     if len(argv) > 1:
-        result = 0
         for argument in argv[1:]:
-            result += int(argument)
+        result += int(argument)
 
-        print('{:d}'.format(result))
+    print'{:d}'.format(result))
