afb37bd3a5be7429f46240e6f3ed6cd5aae6e920
Martin-Rota <martinrota5@gmail.com>
2023-12-18 18:26:36 -0800

    fix indentation error in line 7 for loop

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     result = 0
     if len(argv) > 1:
         for argument in argv[1:]:
-        result += int(argument)
+            result += int(argument)
 
     print'{:d}'.format(result))
