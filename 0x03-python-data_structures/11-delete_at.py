f7df9bf82d70891c3aa4b73c5d1ec427afb51d62
Martin-Rota <martinrota5@gmail.com>
2023-12-27 10:39:23 -0800

    Task 11 first commit

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if len(my_list) > 0:
+    if idx < len(my_list) > 0:
         item = my_list[idx]
         my_list.remove(item)
-
     return my_list
