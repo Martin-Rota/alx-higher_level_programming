598064927e8bd8669bac692864181075b5201c66
Martin-Rota <martinrota5@gmail.com>
2024-01-21 01:20:47 -0800

    Task 16 First Commit

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,42 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p) {
+    if (!PyList_Check(p)) {
+        fprintf(stderr, "Invalid Python List Object\n");
+        return;
+    }
+
+    Py_ssize_t size = PyList_Size(p);
+
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %zd\n", size);
+
+    printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+
+    for (Py_ssize_t i = 0; i < size; ++i) {
+        printf("Element %zd: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+    }
+}
+
+void print_python_bytes(PyObject *p) {
+    if (!PyBytes_Check(p)) {
+        fprintf(stderr, "Invalid Python Bytes Object\n");
+        return;
+    }
+
+    Py_ssize_t size = PyBytes_Size(p);
+    const char *bytes_str = PyBytes_AsString(p);
+
+    printf("[.] bytes object info\n");
+    printf("  [.] Size: %zd\n", size);
+    printf("  [.] Address of the first byte: %p\n", (void *)bytes_str);
+
+    printf("  [.] first %zd bytes: ", (size < 10) ? size : 10);
+    for (Py_ssize_t i = 0; i < 10 && i < size; ++i) {
+        printf("%02hhx", bytes_str[i]);
+        if (i + 1 < (size < 10 ? size : 10)) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
