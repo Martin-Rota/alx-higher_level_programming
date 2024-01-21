5e32693abd2053337152837cf5af60fc7f542c2d
Martin-Rota <martinrota5@gmail.com>
2024-01-21 01:28:10 -0800

    replace Py_TYPE with PyList_GET_ITEM and PyBytes_AS_STRING to address the restrictions provided.

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -6,7 +6,7 @@ void print_python_list(PyObject *p) {
         return;
     }
 
-    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t size = PyObject_Length(p);
 
     printf("[*] Python list info\n");
     printf("[*] Size of the Python List = %zd\n", size);
@@ -14,24 +14,24 @@ void print_python_list(PyObject *p) {
     printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
 
     for (Py_ssize_t i = 0; i < size; ++i) {
-        printf("Element %zd: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+        printf("Element %zd: %s\n", i, Py_TYPE(PyList_GET_ITEM(p, i))->tp_name);
     }
 }
 
 void print_python_bytes(PyObject *p) {
     if (!PyBytes_Check(p)) {
-        fprintf(stderr, "Invalid Python Bytes Object\n");
+        fprintf(stderr, "[ERROR] Invalid Bytes Object\n");
         return;
     }
 
-    Py_ssize_t size = PyBytes_Size(p);
-    const char *bytes_str = PyBytes_AsString(p);
+    Py_ssize_t size = PyObject_Length(p);
+    const char *bytes_str = PyBytes_AS_STRING(p);
 
     printf("[.] bytes object info\n");
     printf("  [.] Size: %zd\n", size);
-    printf("  [.] Address of the first byte: %p\n", (void *)bytes_str);
-
+    printf("  [.] trying string: %s\n", bytes_str);
     printf("  [.] first %zd bytes: ", (size < 10) ? size : 10);
+
     for (Py_ssize_t i = 0; i < 10 && i < size; ++i) {
         printf("%02hhx", bytes_str[i]);
         if (i + 1 < (size < 10 ? size : 10)) {
